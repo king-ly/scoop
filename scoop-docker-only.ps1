bc499e58e5a27e1d59040da9f4fb9b99ba089f1b
king-ly <king-ly@qq.com>
2020-08-01 12:00:19 +0800

    no message

--- scoop-docker-only.ps1
+++ scoop-docker-only.ps1
@@ -1,4 +1,3 @@
-
 # Basic tools
 scoop install 7zip git sudo
 scoop install wget curl
@@ -9,14 +8,7 @@ Write-Host "adding scoop-extras scoop-nonportable scoop-nerd-fonts  bucket..."
 scoop bucket add extras
 scoop bucket add nonportable
 scoop bucket add versions
-scoop bucket add rack https://github.com/racklin/scoop-main-cn.git
 
-#先安装dark、innounp
-scoop install rack/dark
-scoop install rack/innounp
-
-# Editor
-scoop install vim
 
 # Virtualbox / Docker
 scoop install nonportable/virtualbox-with-extension-pack-np
